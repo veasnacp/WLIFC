9a3dc7611f52d7a37b7cfce857bb86da610edc07
Veasna CP <veasnacp910@gmail.com>
2025-12-14 20:56:15 +0700

    Fixing Vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import { Elysia, t } from 'elysia';
-import { staticPlugin } from '@elysiajs/static';
-import { html, Html } from '@elysiajs/html';
-import { WLLogistic } from './wl/edit';
-import { isNumber } from './utils/is';
 import TelegramBot from 'node-telegram-bot-api';
 import { runBot } from './bot';
 
@@ -35,85 +30,6 @@ async function init() {
 if (!isDev) {
   init();
 }
-
-// const app = new Elysia()
-//   .use(
-//     staticPlugin({
-//       prefix: '/',
-//     })
-//   )
-//   .use(html())
-//   .post(
-//     WEBHOOK_PATH,
-//     async ({ body, set }) => {
-//       set.status = 200;
-//       (async () => {
-//         try {
-//           bot.processUpdate(body as TelegramBot.Update);
-//         } catch (error) {
-//           console.error('Error in background processing:', error);
-//         }
-//       })();
-//       return { ok: true };
-//     },
-//     {
-//       body: t.Any(),
-//     }
-//   )
-//   .get('/', ({ html }) => {
-//     return html('<b>Welcome to WL Checker!!!</b>');
-//   })
-//   .get('/wl/*', async ({ params }) => {
-//     const logCode = params['*'];
-//     const isNumeric = isNumber(logCode);
-//     if (!isNumeric) {
-//       return { message: 'Invalid ID', errorCode: 1 };
-//     }
-//     const cookie = process.env.WL_COOKIE || '';
-//     const wl = new WLLogistic(logCode, cookie);
-//     const data = await wl.getDataFromLogCode();
-//     let photos = [] as string[];
-//     if (data && 'message' in data) {
-//       return { message: data.message, errorCode: 1 };
-//     }
-//     if (data && typeof data.warehousing_pic === 'string') {
-//       photos = wl.getPhotoFromData(data);
-//     }
-
-//     return {
-//       message: data ? 'successful' : 'failed',
-//       picLinks: photos,
-//       totalPic: photos.length,
-//       logCode: logCode,
-//       data: !data
-//         ? null
-//         : ({
-//             id: data.id,
-//             pid: data.pid,
-//             unit_price: data.unit_price,
-//             sub_order: data.sub_order,
-//             sub_total: data.sub_total,
-//             total: data.total,
-//             goods_name: data.goods_name,
-//             goods_number: data.goods_number,
-//             member_name: data.member_name,
-//             material: data.material,
-//             volume: data.volume,
-//             volume_record: data.volume_record,
-//             weight: data.weight,
-//             expresstracking: data.expresstracking,
-//             deliveryway: data.deliveryway,
-//             desc: data.desc,
-//           } as typeof data),
-//     };
-//   })
-//   .listen(port, ({ hostname, port }) => {
-//     console.log(
-//       `🦊 Elysia server listening at http://${hostname}:${port}`,
-//       process.env.NODE_ENV
-//     );
-//   })
-//   .compile();
 
 runBot(bot, { webAppUrl });
 export default bot;
