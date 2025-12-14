d4679c550eff8af12bb2a648ecc6cccedf2012bd
Veasna CP <veasnacp910@gmail.com>
2025-12-14 17:23:47 +0700

    Fixing Vercel deployment

--- script.sh
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN="8033680712:AAGka1mJfZ7t7-RhbZ7rH2_Gc0jT6nvnAus"
-VERCEL_DOMAIN="https://wlchecker-bay.vercel.app"
-
-WEBHOOK_URL="https://${VERCEL_DOMAIN}/webhook/${BOT_TOKEN}"
-
-curl -X POST "https://api.telegram.org/bot${BOT_TOKEN}/setWebhook" \
-     -H "Content-Type: application/json" \
-     -d "{\"url\": \"${WEBHOOK_URL}\", \"drop_pending_updates\": true}"
